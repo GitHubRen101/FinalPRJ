dcd4665e70f596f4a95bcf1181da7522a8792e4a
Ren <myschoolmailaddress@gmailcom>
2018-04-24 08:07:36 +0300

    input Courses

--- courses-controller.js
+++ courses-controller.js
@@ -1,49 +1,83 @@
-moviesApp.controller('coursesCtrl', function ($scope, activeUserService, $location, courseService) {
+moviesApp.controller('coursesCtrl', function ($scope, activeUserService, $location, courseService, uniService) {
 
-    // This is an authotization check. If the user is not logged going back to the home screen
-    if (!activeUserService.isLoggedIn()) {
-      $location.path("/");
-      return;
-    }
-  
-  
-    $scope.courses = [];
+  // This is an authotization check. If the user is not logged going back to the home screen
+  if (!activeUserService.isLoggedIn()) {
+    $location.path("/");
+    return;
+  }
 
-    courseService.load(activeUserService.getUser()).then(function () {
-      $scope.courses = courseService.courses;
-    });
 
-    console.log("$scope.coursess:");
-    console.log($scope.courses);
-     
-    // Push new Course to array
-    $scope.addCourse = function(course) {
-      var newCourse = new Course(course);
-      console.log("var newCourse:");
-      console.log(newCourse);
-      $scope.courses.push(newCourse);
-    };
-  
-    // Sorting
-    $scope.sortProp = "";
-    $scope.changeSort = function (propName) {
-      $scope.sortProp = propName;
-    }
-  
-  
-    // Open Course for update
-    $scope.openLecture = function (course) {
-      console.log("open new window to update course:");
-      // console.log('/main/' + $scope.universities.indexOf(uni));
-      // $location.path('/main/' +  $scope.universities.indexOf(uni));
-    }
-  
-  
-  
-  
+  $scope.courses = [];
+  courseService.load(activeUserService.getUser()).then(function () {
+    $scope.courses = courseService.courses;
   });
-  
-  
+
+
+  $scope.universities = [];
+  uniService.load(activeUserService.getUser()).then(function () {
+    $scope.universities = uniService.universities;
+  });
+
+
+
+
+  // Sorting
+  $scope.sortProp = "";
+  $scope.changeSort = function (propName) {
+    $scope.sortProp = propName;
+  }
+
+
+  // Open Course for update
+  $scope.openCourse = function (course) {
+    console.log("open new window to update course:");
+    // console.log('/main/' + $scope.universities.indexOf(uni));
+    // $location.path('/main/' +  $scope.universities.indexOf(uni));
+  }
+
+
+  // formCourse Constructor 
+  function formCourse(courseNum, courseName, courseSyllabus, numOfLectures, semester, uni) {
+    this.courseNum = courseNum;
+    this.courseName = courseName;
+    this.courseSyllabus = courseSyllabus;
+    this.numOfLectures = numOfLectures;
+    this.semester = semester;
+    this.uniName = uni.uniName;
+    this.serialNum = 99999999;
+    this.logicalDelete = false;
+  };
+
+
+  $scope.addFormCourse = function (courseNum, courseName, courseSyllabus, numOfLectures, semester, uni) {
+    console.log("uni:");
+    console.log(uni);
+    console.log("Before Courses:");
+    console.log($scope.courses);
+    var indx;
+    indx = $scope.courses.length - 1;
+    var val;
+    val = $scope.courses[indx].serialNum;
+    var newFormCourse = new formCourse(courseNum, courseName, courseSyllabus, numOfLectures, semester, uni);
+    console.log("var newCourse:");
+    console.log(newFormCourse);
+    newFormCourse.serialNum = val + 1;
+    console.log("After Courses:");
+    console.log($scope.courses);
+    $scope.courses.push(newFormCourse);
+  };
+
+
+
+
+
+
+
+
+
+});
+
+
   // moviesApp.controller('TimeCtrl', function ($scope, $interval) {
   //   var tick = function () {
   //     $scope.clock = Date.now();
@@ -51,4 +85,3 @@ moviesApp.controller('coursesCtrl', function ($scope, activeUserService, $locati
   //   tick();
   //   $interval(tick, 1000);
   // });
-  
--- uni-controller.js
+++ uni-controller.js
@@ -11,18 +11,7 @@ moviesApp.controller('uniCtrl', function ($scope, activeUserService, $location, 
   uniService.load(activeUserService.getUser()).then(function () {
     $scope.universities = uniService.universities;
   });
-
-  console.log("$scope.universities:");
-  console.log($scope.universities);
-
-  // Push new Lecture to array
-  // $scope.addFormUni = function (uni) {
-  //   var newUni = new Uni(uni);
-  //   console.log("var newUni:");
-  //   console.log(newUni);
-  //   $scope.universities.push(newUni);
-  // };
-
+  
 
   // formUni Constructor 
   function formUni(name, address) {
@@ -38,15 +27,8 @@ moviesApp.controller('uniCtrl', function ($scope, activeUserService, $location, 
     indx = $scope.universities.length - 1;
     var val;
     val = $scope.universities[indx].serialNum;
-
-    console.log("val:");
-    console.log(val);
-
-
     var newFormUni = new formUni(name, address);
-
     newFormUni.serialNum = val + 1;
-    console.log(newFormUni);
     $scope.universities.push(newFormUni);
   };
 
@@ -60,13 +42,6 @@ moviesApp.controller('uniCtrl', function ($scope, activeUserService, $location, 
   //   $scope.sortProp = propName;
   // }
 
-
-  // // Open lecture details
-  // $scope.openLecture = function (uni) {
-  //   console.log("go to path:");
-  //   console.log('/main/' + $scope.universities.indexOf(uni));
-  //   $location.path('/main/' +  $scope.universities.indexOf(uni));
-  // }
 
 
 
