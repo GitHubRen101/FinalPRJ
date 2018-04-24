b4cc243e5cedb9133d1a6759f7be0df71d2a78d6
Ren <myschoolmailaddress@gmailcom>
2018-04-24 23:55:25 +0300

    fin

--- lectureDetails-controller.js
+++ lectureDetails-controller.js
@@ -1,7 +1,7 @@
 
-moviesApp.controller("mainLectureDetailsCtrl", function ($scope, $routeParams,  lectureService, activeUserService, $location) {
+moviesApp.controller("mainLectureDetailsCtrl", function ($scope, $routeParams, lectureService, activeUserService, $location, movieService) {
 
-// moviesApp.controller("mainLectureDetailsCtrl", function ($scope, $routeParams, carService, activeUserService, $location) {
+    // moviesApp.controller("mainLectureDetailsCtrl", function ($scope, $routeParams, carService, activeUserService, $location) {
 
     // // This is an authotization check. If the user is not logged going back to the home screen
     // if (!activeUserService.isLoggedIn()) {
@@ -15,8 +15,32 @@ moviesApp.controller("mainLectureDetailsCtrl", function ($scope, $routeParams,  
         $scope.lecture = lectureService.lectures[indexToDisplay];
     })
 
-    // $scope.lecture = carService.cars[indexToDisplay];
-})
+    $scope.movies = [];
+    movieService.load(activeUserService.getUser()).then(function () {
+        $scope.movies = movieService.movies;
+    });
 
+    // filmCuts Constructor 
+    function FilmCut(filmName, filmTheme, filmCutStartHours, filmCutStartMinutes, filmCutStartSeconds, filmCutEndHours, filmCutEndMinutes, filmCutEndSeconds) {
+        this.filmCuts.filmName = filmName;
+        this.filmCuts.theme = filmTheme;
+        this.filmCuts.start.hours = filmCutStartHours;
+        this.filmCuts.start.minutes = filmCutStartMinutes;
+        this.filmCuts.start.seconds = filmCutStartSeconds;
+        this.filmCuts.end.hours = filmCutEndHours;
+        this.filmCuts.end.minutes = filmCutEndMinutes;
+        this.filmCuts.end.seconds = filmCutEndSeconds;
+        this.serialNum = 99999999;
+        this.logicalDelete = false;
+    };
 
-
+    $scope.updateFilmCuts = function (filmName, filmTheme, filmCutStartHours, filmCutStartMinutes, filmCutStartSeconds, filmCutEndHours, filmCutEndMinutes, filmCutEndSeconds) {
+        var indx;
+        indx = $scope.lecture.filmCuts.length - 1;
+        var val;
+        val = $scope.lecture.filmCuts[indx].serialNum;
+        var newFilmCut = new FilmCut(filmName, filmTheme, filmCutStartHours, filmCutStartMinutes, filmCutStartSeconds, filmCutEndHours, filmCutEndMinutes, filmCutEndSeconds);
+        newFilmCut.serialNum = val + 1;
+        $scope.lecture.filmCuts.push(newFilmCut);
+    };
+});
